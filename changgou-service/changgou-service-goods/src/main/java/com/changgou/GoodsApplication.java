11d58994b3ee21430dd3e1281620b45a01e790cb
zcj <2848483541@qq.com>
2022-03-13 22:48:23 +0800

    test

--- changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -6,12 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
-/**
- * @author 三国的包子
- * @version 1.0
- * @package com.changgou *
- * @since 1.0
- */
+
 @SpringBootApplication
 @EnableEurekaClient
 //注意 要使用通用的mapper的组件扫描
